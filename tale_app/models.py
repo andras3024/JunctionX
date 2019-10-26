47f84278dae1b1f2d9e7cbc3d646ee62e3fb87aa
Ludvig Andras <andras.ludvig@yahoo.de>
2019-10-26 12:59:05 +0200

    Database update and migrate

--- tale_app/models.py
+++ tale_app/models.py
@@ -5,6 +5,7 @@ class Tale(models.Model):
     id = models.AutoField(db_column='id', primary_key=True)
     name = models.CharField(db_column='name', max_length=128, blank=True, null=True)
     image = models.ImageField(db_column='image', upload_to='tale_app/pictures', blank=True)
+    level = models.IntegerField(db_column='level', null=False)
 
     class Meta:
         db_table = 'tale'
@@ -17,7 +18,6 @@ class Content(models.Model):
     id = models.AutoField(db_column='id', primary_key=True)
     taleid = models.ForeignKey(Tale, models.CASCADE, db_column='taleid')
     mp4 = models.FileField(db_column='mp4', upload_to='tale_app/videos', null=True)
-    taskid = models.IntegerField(db_column='taskid', null=True, blank=True)
     targetemotion = models.IntegerField(db_column='targetemotion')
     order = models.IntegerField(db_column='order', null=False)
 
@@ -25,4 +25,4 @@ class Content(models.Model):
         db_table = 'content'
 
     def __str__(self):
-        return 'ID:{} ORDER:{}'.format(self.id,self.order)
+        return 'NAME: {} ID:{} ORDER:{}'.format(self.taleid.name, self.id,  self.order)
