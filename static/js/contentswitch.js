1b7dde816b1868c433bc37196bf6d9564b1f1d8e
Ludvig Andras <andras.ludvig@yahoo.de>
2019-10-26 14:53:55 +0200

    Content switch

--- static/js/contentswitch.js
+++ static/js/contentswitch.js
@@ -1,1 +1,14 @@
 console.log('Content Switch');
+const next_content_url = JSON.parse(document.getElementById('next_content_url').textContent);
+var vid = document.getElementById("myVideo");
+
+// Functions on key down events (Enter,Right Arrow,Left Arrow)
+document.onkeydown = function(event) {
+    if (event.key === 'ArrowRight'){
+        window.location.pathname = next_content_url;
+    }
+}
+
+vid.onended = function() {
+   window.location.pathname = next_content_url;
+};
--- tale_app/views.py
+++ tale_app/views.py
@@ -11,6 +11,7 @@ from .models import Content
 from children_app.models import Child
 from statistics_app.models import Session
 from django.utils import timezone
+from next_prev import next_in_order, prev_in_order
 
 
 class TalesList(APIView):
@@ -27,7 +28,7 @@ class TaleStart(APIView):
 
     def get(self, request, **kwargs):
         try:
-            old_session = Session.objects.get(tale=kwargs['tale_id'], child=kwargs['child_id'])
+            old_session = Session.objects.get(tale=kwargs['tale_id'], child=kwargs['child_id'], completed=False)
             first_content_id = old_session.content_id
         except Session.DoesNotExist:
             first_content_id = Content.objects.filter().order_by('order')[0].id
@@ -39,6 +40,11 @@ class TaleStart(APIView):
                 content_id=first_content_id)
             new_session.save()
 
+        try:
+            Content.objects.get(id=first_content_id)
+        except Content.DoesNotExist:
+            first_content_id = Content.objects.filter().order_by('order')[0].id
+
         kwargs.update({'content_id': first_content_id})
         return HttpResponseRedirect(reverse('tale_app:TaleContent', kwargs=kwargs))
 
@@ -47,15 +53,33 @@ class TaleEnd(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, **kwargs):
+        session = Session.objects.get(tale=kwargs['tale_id'], child=kwargs['child_id'], completed=False)
+        session.completed = True
+        session.save()
         tale = Tale.objects.get(id=kwargs['tale_id'])
         context = {'item': tale}
-        return render(request, 'tale_app/tale.html', context)
+        return render(request, 'tale_app/taleend.html', context)
 
 
 class TaleContent(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request, *args, **kwargs):
+        session = Session.objects.get(tale=kwargs['tale_id'], child=kwargs['child_id'], completed=False)
+        contents = Content.objects.filter(taleid=kwargs['tale_id']).order_by('order')
+        curr_content = Content.objects.get(id=kwargs['content_id'])
+        next_content = next_in_order(curr_content, qs=contents)
+        if next_content is None:
+            next_content_url =  reverse('tale_app:TaleEnd', kwargs={
+                'child_id': kwargs['child_id'],
+                'tale_id': kwargs['tale_id'],
+            })
+        else:
+            next_content_url = reverse('tale_app:TaleContent', kwargs={
+                'child_id': kwargs['child_id'],
+                'tale_id': kwargs['tale_id'],
+                'content_id': next_content.id
+            })
         content = Content.objects.get(id=kwargs['content_id'])
-        context = {'item': content}
+        context = {'item': content, 'next_content_url': next_content_url}
         return render(request, 'tale_app/tale.html', context)
