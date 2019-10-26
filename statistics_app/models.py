d6883a7cbda68156d6d700ef76c1d20d1ca53838
Emmert Gergo <emmertgergo@outlook.com>
2019-10-26 12:55:00 +0200

    edit tables

--- statistics_app/models.py
+++ statistics_app/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from children_app.models import Child
-# from tale_app import Tale, Content
+from tale_app.models import Tale, Content
+
+
+class Session(models.Model):
+    id = models.AutoField(db_column='id', primary_key=True)
+    tale = models.ForeignKey(Tale, models.CASCADE)
+    child = models.ForeignKey(Child, models.CASCADE)
+    completed = models.BooleanField(db_column='completed', default=False)
+    date = models.DateTimeField(db_column='date', auto_now=True)
+
+    def __str__(self):
+        return 'ID: {} Child name: {} Tale: {}'.format(self.id, self.child.name, self.tale.name)
 
 
 class Result(models.Model):
     id = models.AutoField(db_column='id', primary_key=True)
-    child = models.ForeignKey(Child, models.CASCADE)
-    # tale = models.ForeignKey(Tale, models.CASCADE)
-    # content = models.ForeignKey(Content, models.CASCADE)
-    target_emotion = models.IntegerField()
+    session = models.ForeignKey(Session, models.CASCADE)
+    content = models.ForeignKey(Content, models.CASCADE)
     emotion_0 = models.FloatField()
     emotion_1 = models.FloatField()
     emotion_2 = models.FloatField()
@@ -23,4 +32,4 @@ class Result(models.Model):
         db_table = 'result'
 
     def __str__(self):
-        return 'ID: {} Child name: {}'.format(self.id, self.child.name)
+        return 'ID: {} Child name: {}'.format(self.id, self.session.child.name)
